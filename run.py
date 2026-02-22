15724b7863ed476a9d7191b5b4b3b0d9604dce6d
Saurabh Thakur <shaurabhnarayan@gmail.com>
2026-02-22 17:34:04 +0530

    Auto-initialize database in run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from backend import create_app
+from backend import create_app, db
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
